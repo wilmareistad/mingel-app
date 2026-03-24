b238065d39298dfca65fb99bd5f35e70815c0e38
Wilma <wilmareistad@icloud.com>
2026-03-24 11:23:35 +0100

    fixed username and trim for code

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -5,53 +5,55 @@ import { nanoid } from "nanoid";
 import { useNavigate } from "react-router-dom";
 
 export default function Join() {
-    const [code, setCode] = useState("");
-    const [username, setUsername] = useState("");
-    const [message, setMessage] = useState("");
-    
-    const navigate = useNavigate();
-    
-    const handleJoin = async () => {
-        if (!code || usename) {
-            return setMessage("Enter code and username");
-        }
-        
-        try {
-            const q = query(
-                collection(db, "events"),
-                where("code", "==", code.toUpperCase())
-            );
-            
-            const snapshot = await getDocs(q);
-            
-            if (snapshot.empty) {
-                return setMessage("Event not found");
-            }
-            
-            const eventDoc = snapshot.docs[0];
-            const eventId = eventDoc.id;
-            
-            const userId = nanoid;
-            
-            await addDoc(collection(db, "users"), {
-                userId,
-                username,
-                eventId,
-                createdAt: serverTimestamp()
-            });
-            
-            localStorage.setItem("userId", userId);
-            localStorage.setItem("eventId", eventId);
-            
-            navigate(`/lobby/${eventId}`);
-            
-        }   catch (error) {
-            console.error(error);
-            setMessage("Something went wrong");
-        }
+  const [code, setCode] = useState("");
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleJoin = async () => {
+    if (!code || !username) {
+      return setMessage("Enter code and username");
     }
-    
-    return (
+
+    try {
+      const cleanCode = code.trim().toUpperCase();
+
+      const q = query(
+        collection(db, "events"),
+        where("code", "==", cleanCode)
+      );
+
+      const snapshot = await getDocs(q);
+
+      if (snapshot.empty) {
+        return setMessage("Event not found");
+      }
+
+      const eventDoc = snapshot.docs[0];
+      const eventId = eventDoc.id;
+
+      const userId = nanoid();
+
+      await addDoc(collection(db, "users"), {
+        userId,
+        username,
+        eventId,
+        createdAt: serverTimestamp()
+      });
+
+      localStorage.setItem("userId", userId);
+      localStorage.setItem("eventId", eventId);
+
+      navigate(`/lobby/${eventId}`);
+
+    } catch (error) {
+      console.error(error);
+      setMessage("Something went wrong");
+    }
+  };
+
+  return (
     <div>
       <h1>Join Game</h1>
 
@@ -73,5 +75,5 @@ export default function Join() {
 
       {message && <p>{message}</p>}
     </div>
-    )
+  );
 }
