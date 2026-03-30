cda43aaa86df72fa2aa51637aae3c6cc14a4ce5d
Wilma <wilmareistad@icloud.com>
2026-03-30 11:50:43 +0200

    added css for header and eventcard and fixed eventcard

--- src/App.jsx
+++ src/App.jsx
@@ -32,14 +32,14 @@ function App() {
   return (
     <>
       {/* Animated background (fixed, behind everything) */}
-      <div className="animated-bg">
-        <div className="gradient gradient1"></div>
-        <div className="gradient gradient2"></div>
-        <div className="gradient gradient3"></div>
-      </div>
 
       {/* Main app content */}
       <BrowserRouter>
+        <div className="animated-bg">
+          <div className="gradient gradient1"></div>
+          <div className="gradient gradient2"></div>
+          <div className="gradient gradient3"></div>
+        </div>
         <Header />
         <main>
           <Routes>
